3804c6962090bfcb9698448bd91948c35ae99ef3
dwu0501 <101563675+dwu0501@users.noreply.github.com>
2022-04-23 00:25:28 -0300

    Validation of password

--- Assets/Script/ValidarContrasenia.cs
+++ Assets/Script/ValidarContrasenia.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ValidarContrasenia : MonoBehaviour
 {
     // Start is called before the first frame update
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text ingresoUsuario;
+
     void Start()
     {
-        
+        contraseniaCorrecta = "12345";
     }
 
     // Update is called once per frame
@@ -17,7 +22,15 @@ public class ValidarContrasenia : MonoBehaviour
     }
     public void validarContrasenia()
         {
-
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña Incorrecta");
+        }
         }
         
     }
