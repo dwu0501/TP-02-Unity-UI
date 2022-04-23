266c0a8eb47d4c2bfb5ab69bd1977bd434c2ab71
dwu0501 <101563675+dwu0501@users.noreply.github.com>
2022-04-22 23:04:14 -0300

    Initial commit

---
+++ Assets/Scripts/MayorDeEdad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayorDeEdad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int edadUsuario;
+    void Start()
+    {
+
+        if (edadUsuario >= 18)
+        {
+
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
